ec05e4cc6c993be238156e9d51d2878545ffa991
Curtis Mitchell <70792951+curtisjm@users.noreply.github.com>
2024-09-19 16:30:13 -0700

    init

---
+++ init.lua
@@ -0,0 +1,2 @@
+require("cjm")
+require("config.lazy")
---
+++ lua/cjm/remap.lua
@@ -0,0 +1,3 @@
+vim.g.mapleader = " "
+-- Set leader pv in normal mode to be the command :Ex that takes us back to netrw (the file explorer)
+vim.keymap.set("n", "<leader>fe", vim.cmd.Ex)
