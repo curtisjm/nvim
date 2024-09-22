0900f9f8cff8a3e9588987b0395d6928c5bfa5cf
Curtis Mitchell <70792951+curtisjm@users.noreply.github.com>
2024-09-21 21:25:35 -0700

    add lsp and redo colorscheme

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -29,8 +29,8 @@ require("lazy").setup({
 	},
 	-- Configure any other settings here. See the documentation for more details.
 	-- colorscheme that will be used when installing plugins.
-	{ "rose-pine/neovim", name = "rose-pine" },
-	install = { colorscheme = { "rose-pine" } },
+	-- { "rose-pine/neovim", name = "rose-pine" },
+	-- install = { colorscheme = { "rose-pine" } },
 	-- automatically check for plugin updates
 	checker = { enabled = true },
 	"nvim-lua/plenary.nvim",
