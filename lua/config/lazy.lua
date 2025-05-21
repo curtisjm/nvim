f97788c90b1ad59b61e18d6bd5d84a708dcf442c
Curtis Mitchell <70792951+curtisjm@users.noreply.github.com>
2025-05-21 12:54:18 -0700

    updated things

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -26,13 +26,16 @@ require("lazy").setup({
 		{ import = "lazyvim.plugins.extras.lang.r" },
 		{ import = "lazyvim.plugins.extras.lang.markdown" },
 		{ import = "lazyvim.plugins.extras.lang.clangd" },
+		{ import = "lazyvim.plugins.extras.lang.go" },
+		-- { import = "lazyvim.plugins.extras.lang.cmake" },
 
 		-- Editor:
 		{ import = "lazyvim.plugins.extras.editor.harpoon2" },
 
 		-- Coding:
 		{ import = "lazyvim.plugins.extras.coding.mini-surround" },
-		{ import = "lazyvim.plugins.extras.coding.copilot-chat" },
+		{ import = "lazyvim.plugins.extras.ai.copilot" },
+		{ import = "lazyvim.plugins.extras.ai.copilot-chat" },
 
 		-- UI:
 		{ import = "lazyvim.plugins.extras.ui.treesitter-context" },
